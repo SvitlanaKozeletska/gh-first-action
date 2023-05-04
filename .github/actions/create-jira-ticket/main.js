0bde947b76717230a4671749de34c56f581c72c5
Svitlana Kozeletska <svitlana_kozeletska_tp@bmc.com>
2023-05-04 03:37:56 +0300

    add input value

--- .github/actions/create-jira-ticket/main.js
+++ .github/actions/create-jira-ticket/main.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 function run () {
 
   const issue = core.getInput('issue', {required: true});
-  core.notice('Hello from my custom JS action' + issue.assignee.login);
+  core.notice('Hello from my custom JS action' + issue);
 }
 
 run();
